365cb61b8607b560aca29cbe70ec0afc1bbd8245
Jakub <kubeckk21@gmail.com>
2023-09-01 22:00:10 +0200

    Styled login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,8 @@
 import TextInput from "@/components/text-input/text-input";
 import { useState } from "react";
 
+import "../register/register.css";
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -12,8 +14,10 @@ export default function Login() {
   };
 
   return (
-    <form className="flex flex-col gap-3 items-center mt-10">
+    <form className="flex flex-col gap-10 items-center mt-10 bg-indigo-200 p-10">
       <TextInput
+        className="register__text-input-wrapper"
+        inputClassName="register__text-input"
         id="email-input"
         label="Email"
         handleOnChange={(event) => setEmail(event.target.value)}
@@ -21,6 +25,8 @@ export default function Login() {
         key="email-input"
       />
       <TextInput
+        className="register__text-input-wrapper"
+        inputClassName="register__text-input"
         id="password-input"
         label="Password"
         handleOnChange={(event) => setPassword(event.target.value)}
@@ -30,9 +36,16 @@ export default function Login() {
       />
 
       <div>
-        <button onClick={handleLogin}>Login</button>
+        <button
+          className="w-60 h-8 border border-black rounded bg-white"
+          onClick={handleLogin}
+        >
+          Login
+        </button>
         <div>
-          <a href="/register">Create an account</a>
+          <a className="text-sm text-link-500 hover:underline" href="/register">
+            Create an account
+          </a>
         </div>
       </div>
     </form>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        link: colors.blue
+      }
     },
   },
   plugins: [],
