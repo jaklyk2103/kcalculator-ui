8dff221e1e2a718196a6d552d050e3c078f35b4c
Jakub <kubeckk21@gmail.com>
2023-09-01 22:07:56 +0200

    Improved form styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@
 import TextInput from "@/components/text-input/text-input";
 import { useState } from "react";
 
-import "../register/register.css";
+import "../register/form.css";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -14,40 +14,42 @@ export default function Login() {
   };
 
   return (
-    <form className="flex flex-col gap-10 items-center mt-10 bg-indigo-200 p-10">
-      <TextInput
-        className="register__text-input-wrapper"
-        inputClassName="register__text-input"
-        id="email-input"
-        label="Email"
-        handleOnChange={(event) => setEmail(event.target.value)}
-        value={email}
-        key="email-input"
-      />
-      <TextInput
-        className="register__text-input-wrapper"
-        inputClassName="register__text-input"
-        id="password-input"
-        label="Password"
-        handleOnChange={(event) => setPassword(event.target.value)}
-        value={password}
-        key="password-input"
-        inputType="password"
-      />
+    <div className="flex w-full justify-center">
+      <form className="flex w-2/3 flex-col gap-10 items-center mt-10 bg-indigo-200 p-10">
+        <TextInput
+          className="register__text-input-wrapper"
+          inputClassName="register__text-input"
+          id="email-input"
+          label="Email"
+          handleOnChange={(event) => setEmail(event.target.value)}
+          value={email}
+          key="email-input"
+        />
+        <TextInput
+          className="register__text-input-wrapper"
+          inputClassName="register__text-input"
+          id="password-input"
+          label="Password"
+          handleOnChange={(event) => setPassword(event.target.value)}
+          value={password}
+          key="password-input"
+          inputType="password"
+        />
 
-      <div>
-        <button
-          className="w-60 h-8 border border-black rounded bg-white"
-          onClick={handleLogin}
-        >
-          Login
-        </button>
         <div>
-          <a className="text-sm text-link-500 hover:underline" href="/register">
-            Create an account
-          </a>
+          <button className="register__button" onClick={handleLogin}>
+            Log in
+          </button>
+          <div>
+            <a
+              className="text-sm text-link-500 hover:underline"
+              href="/register"
+            >
+              Create an account
+            </a>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import TextInput from "@/components/text-input/text-input";
-import "./register.css";
+import "./form.css";
 
 export default function Register() {
   const [firstName, setFirstName] = useState("");
@@ -15,51 +15,50 @@ export default function Register() {
   };
 
   return (
-    <form className="flex flex-col gap-10 items-center mt-10 bg-indigo-200 p-7">
-      <TextInput
-        className="register__text-input-wrapper"
-        inputClassName="register__text-input"
-        id="first-name-input"
-        label="First name"
-        handleOnChange={(event) => setFirstName(event.target.value)}
-        value={firstName}
-        key="first-name-input"
-      />
-      <TextInput
-        className="register__text-input-wrapper"
-        inputClassName="register__text-input"
-        id="email-input"
-        label="Email"
-        handleOnChange={(event) => setEmail(event.target.value)}
-        value={email}
-        key="email-input"
-      />
-      <TextInput
-        className="register__text-input-wrapper"
-        inputClassName="register__text-input"
-        id="password-input"
-        label="Password"
-        handleOnChange={(event) => setPassword(event.target.value)}
-        value={password}
-        key="password-input"
-        inputType="password"
-      />
-      <TextInput
-        className="register__text-input-wrapper"
-        inputClassName="register__text-input"
-        id="confirm-password-input"
-        label="Confirm password"
-        handleOnChange={(event) => setConfirmPassword(event.target.value)}
-        value={confirmPassword}
-        key="confirm-password-input"
-        inputType="password"
-      />
-      <button
-        onClick={handleRegister}
-        className="w-60 h-8 border border-black rounded bg-white"
-      >
-        Register
-      </button>
-    </form>
+    <div className="flex w-full justify-center">
+      <form className="flex flex-col w-2/3 gap-10 items-center mt-10 bg-indigo-200 p-7">
+        <TextInput
+          className="register__text-input-wrapper"
+          inputClassName="register__text-input"
+          id="first-name-input"
+          label="First name"
+          handleOnChange={(event) => setFirstName(event.target.value)}
+          value={firstName}
+          key="first-name-input"
+        />
+        <TextInput
+          className="register__text-input-wrapper"
+          inputClassName="register__text-input"
+          id="email-input"
+          label="Email"
+          handleOnChange={(event) => setEmail(event.target.value)}
+          value={email}
+          key="email-input"
+        />
+        <TextInput
+          className="register__text-input-wrapper"
+          inputClassName="register__text-input"
+          id="password-input"
+          label="Password"
+          handleOnChange={(event) => setPassword(event.target.value)}
+          value={password}
+          key="password-input"
+          inputType="password"
+        />
+        <TextInput
+          className="register__text-input-wrapper"
+          inputClassName="register__text-input"
+          id="confirm-password-input"
+          label="Confirm password"
+          handleOnChange={(event) => setConfirmPassword(event.target.value)}
+          value={confirmPassword}
+          key="confirm-password-input"
+          inputType="password"
+        />
+        <button onClick={handleRegister} className="register__button">
+          Register
+        </button>
+      </form>
+    </div>
   );
 }
