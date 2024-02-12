3dcfe230159ed952b323ec7d47c6d9db6bcf99e1
Jakub Łyko <jlyko@future-processing.com>
2024-02-12 15:03:24 +0100

    Added nvmrc, added tailwindcss nuxt module

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <html lang="en">
+    <body className="{inter.className}">
+      <NavigationBar />
+      <main id="main">
+        <slot />
+      </main>
+    </body>
+  </html>
+</template>
+
+<script setup lang="ts">
+import "./globals.css";
+import NavigationBar from "../components/navigationBar/navigationBar";
+
+export const metadata: Metadata = {
+  title: "Kcalculator App",
+  description: "Manage your nutrition",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavigationBar />
+        <main id="main">{children}</main>
+      </body>
+    </html>
+  );
+</script>
+}
+
+<style></style>
