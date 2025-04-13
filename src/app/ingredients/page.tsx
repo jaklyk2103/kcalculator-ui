8389b1680aedbeae65bd76abcb79ad8cd39e6932
Jakub <kubeckk21@gmail.com>
2025-04-13 10:53:13 +0200

    started developing

--- src/app/ingredients/page.tsx
+++ src/app/ingredients/page.tsx
@@ -1,3 +1,52 @@
+import "./ingredients.css";
+
 export default function Ingredients() {
-  return <div>halo</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-1">
+        <label>Name</label>
+        <input
+          id="ingredients-create__name"
+          className="ingredient-create__input"
+          type="text"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label>Energy</label>
+        <input
+          id="ingredients-create__energy"
+          className="ingredient-create__input"
+          type="text"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label>Proteins</label>
+        <input
+          id="ingredients-create__proteins"
+          className="ingredient-create__input"
+          type="text"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label>Fats</label>
+        <input
+          id="ingredients-create__fats"
+          className="ingredient-create__input"
+          type="text"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label>Carbohydrates</label>
+        <input
+          id="ingredients-create__carbohydrates"
+          className="ingredient-create__input"
+          type="text"
+        />
+      </div>
+    </div>
+  );
 }
